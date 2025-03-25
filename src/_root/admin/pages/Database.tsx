734ada93f10f6806c8d9848302568e25131acf5a
Djonahuti <djonah04@gmail.com>
2025-03-25 14:55:26 +0100

    commit

--- src/_root/admin/pages/Database.tsx
+++ src/_root/admin/pages/Database.tsx
@@ -137,7 +137,7 @@ export const Database = () => {
     <div className="flex flex-1 flex-col">
     <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <Tabs defaultValue="orders">
+          <Tabs defaultValue="users">
             <TabsList>
               <TabsTrigger value="orders">Orders</TabsTrigger>
               <TabsTrigger value="products">Products</TabsTrigger>
